35fcf533aa7453360c5c55615bc85dcf7360732b
rochismo <alexis.platino@gmail.com>
2021-01-04 15:40:20 +0100

    Updated main electron

--- quasar.conf.js
+++ quasar.conf.js
@@ -6,9 +6,9 @@
 // Configuration for your app
 // https://quasar.dev/quasar-cli/quasar-conf-js
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { configure } = require('quasar/wrappers');
+const { configure } = require("quasar/wrappers");
 
-module.exports = configure(function (/* ctx */) {
+module.exports = configure(function(/* ctx */) {
   return {
     // https://quasar.dev/quasar-cli/supporting-ts
     supportTS: true,
@@ -19,14 +19,10 @@ module.exports = configure(function (/* ctx */) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://quasar.dev/quasar-cli/boot-files
-    boot: [
-      
-    ],
+    boot: [],
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
-    css: [
-      'app.scss'
-    ],
+    css: ["app.scss"],
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
@@ -38,13 +34,13 @@ module.exports = configure(function (/* ctx */) {
       // 'line-awesome',
       // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
 
-      'roboto-font', // optional, you are not bound to it
-      'material-icons', // optional, you are not bound to it
+      "roboto-font", // optional, you are not bound to it
+      "material-icons" // optional, you are not bound to it
     ],
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: "hash", // available values: 'hash', 'history'
 
       // transpile: false,
 
@@ -63,8 +59,7 @@ module.exports = configure(function (/* ctx */) {
       // extractCSS: false,
 
       // https://quasar.dev/quasar-cli/handling-webpack
-      extendWebpack (cfg) {
-      },
+      extendWebpack(cfg) {}
     },
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
@@ -76,14 +71,14 @@ module.exports = configure(function (/* ctx */) {
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
     framework: {
-      iconSet: 'material-icons', // Quasar icon set
-      lang: 'en-us', // Quasar language pack
+      iconSet: "material-icons", // Quasar icon set
+      lang: "en-us", // Quasar language pack
       config: {},
 
       // Possible values for "importStrategy":
       // * 'auto' - (DEFAULT) Auto-import needed Quasar components & directives
       // * 'all'  - Manually specify what to import
-      importStrategy: 'auto',
+      importStrategy: "auto",
 
       // For special cases outside of where "auto" importStrategy can have an impact
       // (like functional components as one of the examples),
@@ -107,41 +102,41 @@ module.exports = configure(function (/* ctx */) {
 
     // https://quasar.dev/quasar-cli/developing-pwa/configuring-pwa
     pwa: {
-      workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
+      workboxPluginMode: "GenerateSW", // 'GenerateSW' or 'InjectManifest'
       workboxOptions: {}, // only for GenerateSW
       manifest: {
         name: `Picturer`,
         short_name: `Picturer`,
         description: `A simple tool to save picture urls`,
-        display: 'standalone',
-        orientation: 'portrait',
-        background_color: '#ffffff',
-        theme_color: '#027be3',
+        display: "standalone",
+        orientation: "portrait",
+        background_color: "#ffffff",
+        theme_color: "#027be3",
         icons: [
           {
-            src: 'icons/icon-128x128.png',
-            sizes: '128x128',
-            type: 'image/png'
+            src: "icons/icon-128x128.png",
+            sizes: "128x128",
+            type: "image/png"
           },
           {
-            src: 'icons/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
           },
           {
-            src: 'icons/icon-256x256.png',
-            sizes: '256x256',
-            type: 'image/png'
+            src: "icons/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png"
           },
           {
-            src: 'icons/icon-384x384.png',
-            sizes: '384x384',
-            type: 'image/png'
+            src: "icons/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png"
           },
           {
-            src: 'icons/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
+            src: "icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
           }
         ]
       }
@@ -159,17 +154,15 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://quasar.dev/quasar-cli/developing-electron-apps/configuring-electron
     electron: {
-      bundler: 'packager', // 'packager' or 'builder'
+      bundler: "builder", // 'packager' or 'builder'
 
       packager: {
         // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
-
         // OS X / Mac App Store
         // appBundleId: '',
         // appCategoryType: '',
         // osxSign: '',
         // protocol: 'myapp://path',
-
         // Windows only
         // win32metadata: { ... }
       },
@@ -177,16 +170,31 @@ module.exports = configure(function (/* ctx */) {
       builder: {
         // https://www.electron.build/configuration/configuration
 
-        appId: 'picturer'
+        appId: "picturer",
+        win: {
+          target: "nsis"
+        },
+        nsis: {
+          deleteAppDataOnUninstall: true,
+          allowElevation: true
+        },
+        publish: {
+          provider: "bintray",
+          package: "picturer",
+          repo: "picturer",
+          user: "rochismo",
+          owner: "rochismo",
+          publishAutoUpdate: true
+        }
       },
 
       // More info: https://quasar.dev/quasar-cli/developing-electron-apps/node-integration
       nodeIntegration: true,
 
-      extendWebpack (/* cfg */) {
+      extendWebpack(/* cfg */) {
         // do something with Electron main process Webpack cfg
         // chainWebpack also available besides this extendWebpack
       }
     }
-  }
+  };
 });
--- src-electron/main-process/electron-main.js
+++ src-electron/main-process/electron-main.js
@@ -1,24 +1,71 @@
-import { app, BrowserWindow, nativeTheme, ipcMain } from 'electron'
-import ImageService from './services/image.service';
+import { app, BrowserWindow, nativeTheme, ipcMain, dialog } from "electron";
+import ImageService from "./services/image.service";
+const path = require("path");
 try {
-  if (process.platform === 'win32' && nativeTheme.shouldUseDarkColors === true) {
-    require('fs').unlinkSync(require('path').join(app.getPath('userData'), 'DevTools Extensions'))
+  if (
+    process.platform === "win32" &&
+    nativeTheme.shouldUseDarkColors === true
+  ) {
+    require("fs").unlinkSync(
+      require("path").join(app.getPath("userData"), "DevTools Extensions")
+    );
   }
-} catch (_) { }
-if (process.env.NODE_ENV === 'production') {
-  require('update-electron-app')()
-}
+} catch (_) {}
+const { autoUpdater } = require("electron-updater");
+const log = require("electron-log");
+autoUpdater.logger = log;
+autoUpdater.autoDownload = true;
+autoUpdater.logger.transports.file.level = "info";
 /**
  * Set `__statics` path to static files in production;
  * The reason we are setting it here is that the path needs to be evaluated at runtime
  */
 if (process.env.PROD) {
-  global.__statics = __dirname
+  global.__statics = __dirname;
 }
 
-let mainWindow
+let mainWindow;
+let loading;
+let isExitting = false;
 
-function createWindow () {
+const gotTheLock = app.requestSingleInstanceLock();
+
+if (!gotTheLock) {
+  isExitting = true;
+  app.quit(0);
+} else {
+  app.on("second-instance", (event, commandLine, workingDirectory) => {
+    // Someone tried to run a second instance, we should focus our window.
+    if (loading) {
+      if (loading.isMinimized()) loading.restore();
+      loading.focus();
+    } else if (mainWindow) {
+      createWindow();
+    }
+  });
+
+  // Create myWindow, load the rest of the app, etc...
+  //app.whenReady().then(displayautoUpdater);
+  //app.on("activate", displayautoUpdater);
+}
+
+function loadError(error) {
+  log.error(error);
+  if (loading) {
+    loading.webContents.send("update-not-found");
+  }
+  setTimeout(() => {
+    if (!mainWindow) {
+      createWindow();
+    }
+
+    if (loading) {
+      loading.close();
+    }
+  }, 2000);
+}
+
+function createWindow() {
   /**
    * Initial window options
    */
@@ -32,52 +79,95 @@ function createWindow () {
       // Change from /quasar.conf.js > electron > nodeIntegration;
       // More info: https://quasar.dev/quasar-cli/developing-electron-apps/node-integration
       nodeIntegration: true,
-      nodeIntegrationInWorker: true,
+      nodeIntegrationInWorker: true
 
       // More info: /quasar-cli/developing-electron-apps/electron-preload-script
       // preload: path.resolve(__dirname, 'electron-preload.js')
     }
-  })
+  });
 
-  mainWindow.loadURL(process.env.APP_URL)
+  mainWindow.loadURL(process.env.APP_URL);
 
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
-  
-  ipcMain.on('add-image', async (_, url) => {
-    const imageData = await imageService.add(url)
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+
+  ipcMain.on("add-image", async (_, url) => {
+    const imageData = await imageService.add(url);
     mainWindow.webContents.send("image-added", imageData);
   });
 
-  ipcMain.on('get-images', () => {
-    mainWindow.webContents.send("all-images", imageService.getAll())
-  })
+  ipcMain.on("get-images", () => {
+    mainWindow.webContents.send("all-images", imageService.getAll());
+  });
 
   ipcMain.on("delete-image", (ev, id) => {
     imageService.remove(id);
-  })
+  });
 
   ipcMain.on("update-image", async (ev, img) => {
     const updatedImage = await imageService.update(img);
-    mainWindow.webContents.send(`updated-${img.id}`, updatedImage)
+    mainWindow.webContents.send(`updated-${img.id}`, updatedImage);
   });
 
   ipcMain.on("set-favorite", (ev, img) => {
     imageService.setFavorite(img);
-  })
+  });
 }
 
-app.on('ready', createWindow)
+autoUpdater.on("error", loadError);
+autoUpdater.on("update-downloaded", () => {
+  autoUpdater.quitAndInstall(true, true);
+});
+autoUpdater.on("update-available", () => {
+  loading.webContents.send("update-available");
+});
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+autoUpdater.on("update-not-available", () => {
+  loading.webContents.send("no-updates");
+  setTimeout(() => {
+    createWindow();
+    loading.close();
+  }, 2000);
+});
+
+autoUpdater.on("download-progress", ({ percent, transferred, total }) => {
+  loading.webContents.send("downloading-update", percent);
+});
+
+app.on("ready", () => {
+  if (process.env.NODE_ENV !== "development") {
+    return createWindow();
   }
-})
+  loading = new BrowserWindow({
+    width: 400,
+    height: 500,
+    show: false,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
 
-app.on('activate', () => {
+  loading.once("show", () => {
+    autoUpdater.checkForUpdatesAndNotify();
+  });
+  // We are done, load electron
+  loading.on("close", () => {
+    loading = null;
+  });
+  loading.loadURL(path.join(__statics, "load-bar/index.html"));
+  loading.show();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
